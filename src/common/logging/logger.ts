dbe9ba9b9c85c8257df7dc24310376e5b702bf38
Ari Choudhury <arijit.choudhury@outlook.com>
2025-01-02 11:23:00 -0800

    Update logger.ts

--- src/common/logging/logger.ts
+++ src/common/logging/logger.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Aruna Labs, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as fs from 'fs';
 import * as path from 'path';
 import { ExtensionContext } from 'vscode';
