ea78d6bd4af38018871a910a184239cc294cc617
Ari Choudhury <arijit.choudhury@outlook.com>
2025-01-20 15:43:46 -0800

    Stream explanation text

--- src/capabilities/chat/response/file.change.response.processor.ts
+++ src/capabilities/chat/response/file.change.response.processor.ts
@@ -29,7 +29,7 @@ export class FileChangeResponseProcessor {
 		prefix: string;
 		endPrefix: string;
 		onStart?: (value: string) => void;
-		onToken?: (value: string) => void;
+		onToken?: (currentToken: string, fullToken: string) => void;
 		onEnd?: (value: string) => void;
 		streaming?: boolean;
 	}> = {
@@ -39,32 +39,33 @@ export class FileChangeResponseProcessor {
 				onStart: () => {
 					this.textProcessor.processLine(FILE_CHANGE_START);
 				},
-				onToken: (value: string) => {
-					this.textProcessor.processLine(`${FILE_PATH_START}${value}${FILE_PATH_END}`);
+				onToken: () => {
 				},
-				onEnd: () => { }
+				onEnd: (value: string) => {
+					this.textProcessor.processLine(`${FILE_PATH_START}${value}${FILE_PATH_END}`);
+				}
 			},
 			language: {
 				prefix: '"language":"',
 				endPrefix: '","fileAction":',
 				onStart: () => { },
-				onToken: (value: string) => {
+				onToken: () => {},
+				onEnd: (value: string) => {
 					this.textProcessor.processLine(`${LANGUAGE_START}${value}${LANGUAGE_END}`);
-				},
-				onEnd: () => { }
+				}
 			},
 			fileAction: {
 				prefix: '"fileAction":"',
 				endPrefix: '","updateAction":',
 				onStart: () => { },
-				onToken: (value: string) => { },
+				onToken: () => { },
 				onEnd: () => { }
 			},
 			updateAction: {
 				prefix: '"updateAction":"',
 				endPrefix: '","searchContent":',
 				onStart: () => { },
-				onToken: (value: string) => { },
+				onToken: () => { },
 				onEnd: () => { }
 			},
 			searchContent: {
@@ -73,12 +74,11 @@ export class FileChangeResponseProcessor {
 				onStart: () => {
 					this.textProcessor.processLine(SEARCH_START);
 				},
-				onToken: (value: string) => {
+				onToken: () => {},
+				onEnd: (value: string) => {
 					value.split('\n').forEach((line) => {
 						this.textProcessor.processLine(line);
 					});
-				},
-				onEnd: () => {
 					this.textProcessor.processLine(SEARCH_END);
 				}
 			},
@@ -90,8 +90,8 @@ export class FileChangeResponseProcessor {
 					this.textProcessor.processLine(REPLACE_START);
 					this.textProcessor.processToken(value);
 				},
-				onToken: (value: string) => {
-					this.textProcessor.processToken(value
+				onToken: (currentToken: string, fullToken: string) => {
+					this.textProcessor.processToken(fullToken
 						.replace(/\\t/g, '\t')
 						.replace(/\\n/g, '\n')
 						.replace(/\\r/g, '\r')
@@ -122,11 +122,14 @@ export class FileChangeResponseProcessor {
 			explanation: {
 				prefix: '"explanation":"',
 				endPrefix: '","end_change":',
+				streaming: true,
 				onStart: () => {
 					this.markdownRenderer.startMarkdownBlock();
 				},
-				onToken: (value: string) => this.markdownRenderer.processMarkdownToken(value),
-				onEnd: (value) => {
+				onToken: (currentToken: string, fullToken: string) => {
+					this.markdownRenderer.processMarkdownToken(currentToken);
+				},
+				onEnd: (value: string) => {
 					this.markdownRenderer.endMarkdownBlock();
 				}
 			},
@@ -158,6 +161,10 @@ export class FileChangeResponseProcessor {
 				this.buffer = this.buffer.substring(start);
 				this.currentToken = { type, value: initialValue };
 				config.onStart?.(initialValue);
+				// If this is a streaming token type, process the initial value
+				if (config.streaming) {
+					config.onToken?.(initialValue, initialValue);
+				}
 				return true;
 			}
 		}
@@ -176,13 +183,13 @@ export class FileChangeResponseProcessor {
 
 			// Stream tokens if the config specifies streaming
 			if (config.streaming) {
-				config.onToken?.(this.currentToken.value);
+				config.onToken?.(token, this.currentToken.value);
 			}
 
 			if (this.currentToken.value.includes(config.endPrefix)) {
 				const endIndex = this.currentToken.value.indexOf(config.endPrefix);
 
-				// Only parse and call onToken for non-streaming types
+				// Only parse and call onEnd for non-streaming types
 				if (!config.streaming) {
 					try {
 						// First unescape any escaped quotes that are part of the content
@@ -196,13 +203,13 @@ export class FileChangeResponseProcessor {
 							.replace(/\\n/g, '\n')
 							.replace(/\\t/g, '\t');
 							
-						config.onToken?.(value);
+						config.onEnd?.(value);
 					} catch (error) {
 						console.error('Error parsing token:', error);
 					}
+				} else {
+					config.onEnd?.(this.currentToken.value.substring(0, endIndex));
 				}
-
-				config.onEnd?.(this.currentToken.value.substring(0, endIndex));
 
 				const tokenValue = this.currentToken.value;
 				this.currentToken = null;
